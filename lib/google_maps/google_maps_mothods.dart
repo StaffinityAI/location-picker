acc13ee131acffa084e23346ff898b5b689c3334
Viral Kachhadiya <kachhadiya123viral@gmail.com>
2024-10-30 14:49:37 +0530

    reverse geocoding api add for google address search api.

--- lib/google_maps/google_maps_mothods.dart
+++ lib/google_maps/google_maps_mothods.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart' as http;
+import 'package:latlong2/latlong.dart';
 import 'package:location_picker/extension/google_geocoding_address.dart';
 
 googlePlacesSearch(
@@ -23,6 +24,23 @@ googlePlacesSearch(
           countryText: 'Germany',
           streetText: street,
           zipcodeText: zipcode);
+      return add;
+    }
+  } catch (e) {
+    throw e.toString();
+  }
+}
+
+googlePlacesReverseGeoSearch(LatLng points, String apiKey) async {
+  try {
+    Uri url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=${points.latitude},${points.longitude}&key=$apiKey');
+    log('URL::${url.toString()}');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> add;
+      add = (json.decode(response.body) as Map<dynamic, dynamic>)
+          .getAddressFromResponse(cityText: "", streetNoText: "", countryText: '', streetText: "", zipcodeText: "");
       return add;
     }
   } catch (e) {
