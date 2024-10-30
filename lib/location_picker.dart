f59cc92c9b78fa602cc18cb6857827afa7f2a6e1
Viral Kachhadiya <kachhadiya123viral@gmail.com>
2024-10-30 12:25:46 +0530

    Nominatim package method added.

--- lib/location_picker.dart
+++ lib/location_picker.dart
@@ -6,4 +6,40 @@ import 'package:firebase_cloud_firestore/firebase_cloud_firestore.dart';
 import 'package:geoflutterfire_plus/geoflutterfire_plus.dart';
 import 'package:osm_nominatim/osm_nominatim.dart';
 
-class LocationPicker {}
+class LocationPicker {
+  static Future<Map<String, dynamic>> searchByName({
+    required String street,
+    required String zipcode,
+  }) async {
+    try {
+      final searchResult = await Nominatim.searchByName(
+          street: street,
+          postalCode: zipcode,
+          country: 'Germany',
+          limit: 1,
+          addressDetails: true,
+          extraTags: true,
+          nameDetails: true);
+      if (searchResult.isEmpty || searchResult[0].address == null) {
+        throw 'No Address Found!';
+      }
+      final addressMap = searchResult[0].address!;
+
+      final geoPoint = GeoPoint(searchResult[0].lat, searchResult[0].lon);
+      final GeoFirePoint geoFirePoint = GeoFirePoint(geoPoint);
+      log('SELECTED ADDRESS:: $addressMap');
+
+      return {
+        'street': addressMap['road'],
+        'streetNumber': addressMap['house_number'],
+        'zipcode': addressMap['postcode'],
+        'city': addressMap['city'],
+        'country': addressMap['country'],
+        'geoPoint': geoPoint,
+        'geohash': geoFirePoint.data['geohash'],
+      };
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
