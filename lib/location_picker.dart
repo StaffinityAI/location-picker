2cf1b9db4c6d346264bd258127fd6c3c88c6efc0
Viral Kachhadiya <kachhadiya123viral@gmail.com>
2024-10-30 14:42:55 +0530

    common methods add for address search function.

--- lib/location_picker.dart
+++ lib/location_picker.dart
@@ -1,5 +1,6 @@
 library location_picker;
 
+import 'package:latlong2/latlong.dart';
 import 'package:location_picker/nominatim/nominatim_methods.dart';
 
 import 'google_maps/google_maps_mothods.dart';
@@ -20,6 +21,17 @@ class LocationPicker {
       return googlePlacesSearch(
           street: street, streetNumber: streetNumber, city: city, zipcode: zipcode, apiKey: googleMapsApiKey!);
     }
+  }
+
+  static pickLocationFromLatLong(
+      {required LatLng points,
+
+      /// ONLY REQUIRED WHEN USING [PickerMethods.googleMapsMethod]
+      String? googleMapsApiKey,
+      required PickerMethods method}) {
+    if (method == PickerMethods.nominatimMethod) {
+      return reverseGeocoding(points);
+    } else if (method == PickerMethods.googleMapsMethod) {}
   }
 }
 
