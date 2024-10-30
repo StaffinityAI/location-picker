9b42ca0ca1510251cf2abc2bf2172d5b83e47cf5
Viral Kachhadiya <kachhadiya123viral@gmail.com>
2024-10-30 12:25:02 +0530

    package created & dependency added for packages.

---
+++ lib/location_picker.dart
@@ -0,0 +1,9 @@
+library location_picker;
+
+import 'dart:developer';
+
+import 'package:firebase_cloud_firestore/firebase_cloud_firestore.dart';
+import 'package:geoflutterfire_plus/geoflutterfire_plus.dart';
+import 'package:osm_nominatim/osm_nominatim.dart';
+
+class LocationPicker {}
---
+++ test/location_picker_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:location_picker/location_picker.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = LocationPicker();
+  });
+}
